e089e383dfe4967bccf313f23be0f3db3e107f72
Nicholas Lamb <nickplamb@gmail.com>
2021-07-23 13:43:19 -0400

    add all tests and functionality for first two scenarios of the first feature.
    Feature 1: FILTER EVENTS BY CITY
    * Scenario 1: When user hasn’t searched for a city, show upcoming events from all cities
    * Scenario 2: User should see a list of suggestions when they search for a city

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { expect } from '@jest/globals';
+import { beforeAll, expect } from '@jest/globals';
 
 import App from '../App';
 import EventList from '../components/EventList'
@@ -8,13 +8,16 @@ import CitySearch from '../components/CitySearch'
 
 describe('<App /> component', () => {
 
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  })
+
   test('render event list component', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
 
   test('render CitySearch', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 });
--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function CitySearch() {
-  return (
-    <div className="CitySearch">
+export default class CitySearch extends Component {
+  state = {
+    query: '',
+    suggestions: [],
+  }
 
-    </div>
-  )
+  handleInputChanged = e => {
+    const value = e.target.value;
+    const suggestions = this.props.locations.filter(location => {
+      return location.toUpperCase().indexOf(value.toUpperCase())> -1;
+    });
+    this.setState({ 
+      query: value,
+      suggestions,
+    });
+  }
+
+  render() {
+    return (
+      <div className="CitySearch">
+        <input 
+          type="text"
+          className="city"
+          value={ this.state.query }
+          onChange={ this.handleInputChanged }
+        />
+        <ul className="suggestions" >
+          {this.state.suggestions.map(suggestion => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+          <li key="all">
+            <b>See all cities</b>
+          </li>
+        </ul>
+  
+      </div>
+    );
+  }
 }
