f7756eed8e1ecb8cec3e0fbd491a7cb64d4a8f6c
Nicholas Lamb <nickplamb@gmail.com>
2021-07-23 14:23:21 -0400

    add test and functionality to change query state to selected suggestion

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -17,6 +17,10 @@ export default class CitySearch extends Component {
     });
   }
 
+  handleItemClicked = suggestion => {
+    this.setState({ query: suggestion })
+  }
+
   render() {
     return (
       <div className="CitySearch">
@@ -28,7 +32,10 @@ export default class CitySearch extends Component {
         />
         <ul className="suggestions" >
           {this.state.suggestions.map(suggestion => (
-            <li key={suggestion}>{suggestion}</li>
+            <li 
+              key={ suggestion }
+              onClick={ () => this.handleItemClicked(suggestion) }
+            >{ suggestion }</li>
           ))}
           <li key="all">
             <b>See all cities</b>
