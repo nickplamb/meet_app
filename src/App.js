56beb9a04c8ad3583b044c483699cb313da01453
Nicholas Lamb <nickplamb@gmail.com>
2021-07-25 13:15:42 -0400

    change App.js to class based component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 
-function App() {
-  return (
-    <div className="App">
-     <CitySearch />
-     <EventList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <CitySearch />
+       <EventList />
+      </div>
+    );
+  }
 }
 
 export default App;
