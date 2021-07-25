746012d9c7cf999fe575b7aae2f0bccb9ea432d6
Nicholas Lamb <nickplamb@gmail.com>
2021-07-25 14:06:52 -0400

    add tests and functionality for taking user input for number of events and setting it to state

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import NumberOfEvents from '../components/NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+  
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+  test('render input for number of events', () => {
+    expect(NumberOfEventsWrapper.find('input.number-of-events__input')).toHaveLength(1);
+  });
+
+  test('input should be of type number', () => {
+    expect(NumberOfEventsWrapper.find('input.number-of-events__input').prop('type')).toBe('number');
+  });
+
+  test('number in state should default to 32', () => {
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
+  });
+
+  test('change state to number inputed into input', () => {
+    const eventObject = { target: { value: 12 } };
+    NumberOfEventsWrapper.find('.number-of-events__input').simulate('change', eventObject);
+
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(12);
+  });
+
+});
--- src/components/NumberOfEvents.jsx
+++ src/components/NumberOfEvents.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
 class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32,
+  };
+
+  handleNumberChange = e => {
+    const value = e.target.value;
+    this.setState({
+      numberOfEvents: value,
+    });
+  }
+
   render() {
     return (
-      <div>
-
+      <div className="number-of-events">
+        <label htmlFor="number-of-events__input" >Number of events:</label>
+        <input 
+          type="number"
+          id="number-of-events__input"
+          className="number-of-events__input" 
+          onChange={e => this.handleNumberChange(e)}
+        />
       </div>
     )
   }
