162fc45c0d04a368163cca117146183e90aba0dd
Nicholas Lamb <nickplamb@gmail.com>
2021-07-25 20:23:34 -0400

    intergrate serverless functions for OAuth and google calendar into api.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import './nprogress.css';
 
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
--- src/api.js
+++ src/api.js
@@ -1,4 +1,89 @@
+import axios from 'axios';
+import NProgress from 'nprogress';
+
 import { mockData } from "./mock-data";
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get(
+        'https://r1t3t9efoh.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url'
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+const checkToken = async accessToken => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then(res => res.json())
+    .catch(error => error.json());
+  
+    return result;
+};
+
+export const getEvents = async () => {
+  NProgress.start();
+
+  if (window.location.href.startsWith('http://localhost')) {
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = `https://r1t3t9efoh.execute-api.us-east-2.amazonaws.com/dev/api/get-events/${token}`;
+    const result = await axios.get(url);
+    if (result.data) {
+      let locations = extractLocations(result.data.events);
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
+    }
+    NProgress.done();
+
+    return result.data.events;
+  }
+};
+
+const removeQuery = () => {
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl = 
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState('', '', newurl);
+  } else {
+    newurl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
+  }
+};
+
+const getToken = async code => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    `https://r1t3t9efoh.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`
+  )
+    .then(res => res.json())
+    .catch(error => error);
+  
+    access_token && localStorage.setItem('access_token', access_token)
+
+    return access_token;
+};
 
 /**
  * 
@@ -25,8 +110,4 @@ export const extractDateTime = event => {
   const dateTimeOptions = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric', timeZoneName: 'long' };
   const eventDateTime = new Date(event.start.dateTime).toLocaleString([], dateTimeOptions);
   return eventDateTime;
-};
-
-export const getEvents = async () => {
-  return mockData;
 };
